cf7efe5d3c17cd36d9aadee258d29a2465af475a
swappy2122 <patankarswarup21@gmail.com>
2025-02-13 12:03:52 +0530

    Edited

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.use(express.static('public')); // Serve static files from the 'public' directory
+// Serve static files from the 'public' folder
+app.use(express.static('publicc'));
 
+// Home Route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'publicc/html.html');
+});
+
+// Cart Route
 app.get('/cart', (req, res) => {
-    res.send('This is your cart page.');
+    res.send('Your cart is empty.');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
